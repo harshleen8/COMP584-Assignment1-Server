969251ae4add7adbeb29fca8342dfc6d164439f8
Harshleen <harshleensadhnani8@gmail.com>
2023-04-09 13:54:41 -0700

    Added City controller

---
+++ Controllers/CityController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Atalnta", "New York","Los Angeles","Seattle" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Atalnta";
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
